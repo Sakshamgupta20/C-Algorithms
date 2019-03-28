f947031e5b8b69f54e7882bcc9d1c1f82db4dccc
Sakshamgupta20 <sg20saksham@gmail.com>
2019-03-28 18:52:06 +0530

    Delete function added

--- Binary_Search_Tree.cpp
+++ Binary_Search_Tree.cpp
@@ -59,6 +59,50 @@ struct node* search(struct node *node,int data)
 		return(search(node->right,data));
 	}
 }
+
+struct node * minValueNode(struct node* node) 
+{ 
+    struct node* current = node; 
+    while (current->left != NULL) 
+	current = current->left; 
+	return current; 
+} 
+
+struct node *deleteNode(struct node *root,int data)
+{
+	if(root==NULL)
+	{
+		return root;
+	}
+	
+	if(data<root->data)
+	{
+		root->left=deleteNode(root->left,data);
+	}
+	else if(data>root->data)
+	{
+		root->right=deleteNode(root->right,data);
+	}
+	else
+	{
+	if(root->left==NULL)
+	{
+		struct node *temp=root->right;
+		free(root);
+		return(temp);
+	}
+	if(root->right==NULL)
+	{
+		struct node *temp=root->left;
+		free(root);
+		return(temp);
+	}
+	struct node *temp=minValueNode(root->right);
+	root->data=temp->data;
+	root->right=deleteNode(root->right, temp->data); 
+}
+return root;
+}
 int main()
 {
 	struct node *root=NULL;
@@ -71,5 +115,21 @@ int main()
     insert(root,80);	
     inorder(root);
     
-    search(root,20);
+    printf("Inorder traversal of the given tree \n"); 
+    inorder(root); 
+  
+    printf("\nDelete 20\n"); 
+    root = deleteNode(root, 20); 
+    printf("Inorder traversal of the modified tree \n"); 
+    inorder(root); 
+  
+    printf("\nDelete 30\n"); 
+    root = deleteNode(root, 30); 
+    printf("Inorder traversal of the modified tree \n"); 
+    inorder(root); 
+  
+    printf("\nDelete 50\n"); 
+    root = deleteNode(root, 50); 
+    printf("Inorder traversal of the modified tree \n"); 
+    inorder(root); 
 }
